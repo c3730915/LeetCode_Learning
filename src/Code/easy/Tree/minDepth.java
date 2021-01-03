8d53810a431bd46f66ab3630ca2fcfa4110e3adb
c3730915 <1023700418@qq.com>
2021-01-03 10:55:32 +0800

    null

---
+++ src/Code/easy/Tree/minDepth.java
@@ -0,0 +1,29 @@
+package Code.easy.Tree;
+
+public class minDepth {
+    public int solution_DeptFirst(TreeNode root)
+    {
+        /**
+         * 使用深度优先遍历
+         */
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(solution_DeptFirst(root.left), min_depth);
+        }
+        if (root.right != null) {
+            min_depth = Math.min(solution_DeptFirst(root.right), min_depth);
+        }
+
+        return min_depth + 1;
+    }
+
+
+}
